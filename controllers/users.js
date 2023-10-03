7ccd095311e6d5136f45c7760edc63d161362f6a
alees <nprolol8@gmail.com>
2023-10-03 16:46:32 +0300

    mongodb

--- controllers/users.js
+++ controllers/users.js
@@ -50,6 +50,10 @@ module.exports.getUserById = async (req, res) => {
 module.exports.updateUserMe = async (req, res) => {
   try {
     const { name, about } = req.body;
+    if (name.length < 2 || name.length > 30 || about.length < 2 || about.length > 30) {
+      res.status(ERROR_CODE_VALIDATION).send({ message: 'Переданы некорректные данные при обновлении профиля' });
+      return;
+    }
     const users = await User.findByIdAndUpdate(req.user._id, { name, about }, { new: true });
     if (!users) {
       res.status(ERROR_CODE_VALIDATION).send({ message: 'Переданы некорректные данные при обновлении профиля' });
